94146b6339ac2ab9b0338b11c20f95c09018654b
waspinator <patrickwasp@gmail.com>
2018-06-07 12:20:41 -0400

    removed lua and matlab

--- setup.py
+++ setup.py
@@ -9,16 +9,18 @@ import numpy as np
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        sources=['common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs=[np.get_include(), 'common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(name='pycocotools',
       packages=['pycocotools'],
-      package_dir = {'pycocotools': 'pycocotools'},
-      version='2.0',
-      ext_modules=
-          cythonize(ext_modules)
+      package_dir={'pycocotools': 'pycocotools'},
+      version='2.1',
+      ext_modules=cythonize(ext_modules),
+      install_requires=[
+          'numpy', 'cython'
+      ]
       )
